bb21abddbe8eb0a8f20f1974178b8eccdc8e9012
Ilya Bazhenov <31971067+bazhil@users.noreply.github.com>
2018-10-23 20:09:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const request = new XMLHttpRequest();
+
+request.addEventListener('load', onload);
+request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', true);
+request.send();
+
+function onload({target}) {
+  if (target.status === 200) {
+    const response = JSON.parse(target.responseText);
+    setData(response);
+  }
+}
